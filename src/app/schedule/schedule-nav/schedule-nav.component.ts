3c65767d3d9dfaf523dd0465ed926eacbb1e66e6
Zak Eddington <zak.eddington@wearepop.com>
2019-09-19 14:33:42 -0700

    fix datepicker position

--- src/app/schedule/schedule-nav/schedule-nav.component.ts
+++ src/app/schedule/schedule-nav/schedule-nav.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { ActivatedRoute, ParamMap } from '@angular/router';
 import { CONSTANTS } from '../../config/Constants';
 import * as moment from 'moment';
 import { ScheduleService } from '../../services/schedule.service';
